e7483af9a025b0a3cae7f7ccc54951cf6a1a4946
muhsinct <56577918+muhsinct@users.noreply.github.com>
2021-07-29 11:05:27 +0530

    Update urls.py

--- project_main/urls.py
+++ project_main/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('teachers/', include('teacher_profile.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
